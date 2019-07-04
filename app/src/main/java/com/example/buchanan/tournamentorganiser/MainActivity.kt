bc2d8591cbe97f29a88367319e30758591050f31
Phyrik <34688504+Phyrik@users.noreply.github.com>
2019-07-04 16:56:10 +0100

    Started work on ListView on the homepage

--- app/src/main/java/com/example/buchanan/tournamentorganiser/MainActivity.kt
+++ app/src/main/java/com/example/buchanan/tournamentorganiser/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.buchanan.tournamentorganiser
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
 import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +26,9 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, NewTournamentPart1::class.java)
             startActivity(intent)
         }
+
+        val tournamentListView = findViewById<ListView>(R.id.tournamentList)
+        tournamentListView.adapter = TournamentListViewAdapter(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -37,6 +46,35 @@ class MainActivity : AppCompatActivity() {
 
         else -> {
             super.onOptionsItemSelected(item)
+        }
+    }
+
+    private class TournamentListViewAdapter(context: Context) : BaseAdapter() {
+
+        private val mContext: Context
+
+        init {
+            mContext = context
+        }
+
+        //responsible for number of rows in list
+        override fun getCount(): Int {
+            return 5
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getItem(position: Int): Any {
+            return "TEST STRING"
+        }
+
+        //renders each row
+        override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
+            val textView = TextView(mContext)
+            textView.text = "A row"
+            return textView
         }
     }
 }
--- app/src/main/java/com/example/buchanan/tournamentorganiser/ViewTournament.kt
+++ app/src/main/java/com/example/buchanan/tournamentorganiser/ViewTournament.kt
@@ -1,9 +1,11 @@
 package com.example.buchanan.tournamentorganiser
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.TextView
 import java.io.BufferedReader
 import java.io.FileInputStream
@@ -19,6 +21,12 @@ class ViewTournament : AppCompatActivity() {
         val titleTextView = findViewById<TextView>(R.id.titleTextView)
         val titleString = intent.getStringExtra("title")
         titleTextView.text = titleString
+
+        val backButton = findViewById<Button>(R.id.button)
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
